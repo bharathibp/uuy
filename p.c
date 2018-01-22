6904e51e23598505ff2613946e137c6cda0d3b3e
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-01-22 16:58:59 +0530

    p.c

---
+++ p.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	double number;
+	printf("enter a number");
+	scanf("%f",&number);
+	if(number<0.0)
+	printf("is nagative number")
+	else if
+	printf("is passtive number");
+	else
+	printf("is 0");
+	
+	
+	
+	// your code goes here
+	return 0;
+}
