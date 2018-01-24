df8c57fcb36f408f8333417d0e36110f91b27821
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-01-24 15:36:11 +0530

    pp.c

---
+++ pp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+double number;
+printf("enter a number");
+scanf("%f",&number);
+if(number<0.0)
+printf("is nagative number")
+else if
+printf("is passtive number");
+	else
+printf("is 0");
+	return 0;
+}
