7065d453a301aca5aad3a08aa0317137dc5f1cef
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-01-24 16:39:46 +0530

    nature.c

---
+++ nature.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	 int n, i,sum = 0;
+
+    printf("Enter the value of n(positive integer) ");
+    scanf("%d",&n);
+
+    for(i=1; i <= n; i++)
+    {
+       sum = sum + i;
+    }
+
+    printf("Sum of  natural numbers is %d",n, sum);
+
+	
+	return 0;
+}
