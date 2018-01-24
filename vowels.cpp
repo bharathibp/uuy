0ef5b46b055be90c33bc4e8a601edcee7c174697
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-01-24 15:44:47 +0530

    vowels.cpp

---
+++ vowels.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void vowelorConsonant(char x)
+{
+    if (x == 'a' || x == 'e' || x == 'i' ||
+                    x == 'o' || x == 'u')
+    cout << "Vowel" << endl;
+    else
+     cout << "Consonant" << endl;
+}
+getch(); 
+ 
