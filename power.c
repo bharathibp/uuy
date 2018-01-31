78c373d76da3d2daece690c566e266a149c1a577
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-01-31 15:48:29 +0530

    power.c

---
+++ power.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+#include <math.h> 
+ 
+int main()
+{
+    double base, expo, power;
+    printf("Enter base: ");
+    scanf("%d", &base);
+    printf("Enter exponent: ");
+    scanf("%d", &expo);
+    power = pow(base, expo);
+ 
+    printf("%d%d%d", base, expo, power);
+ 
+    return 0;
+}
