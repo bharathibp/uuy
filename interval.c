75d94724e3d3933ec0b3e0bf9ac776b7d7e98e90
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-01-31 16:29:59 +0530

    interval.c

---
+++ interval.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {  
+    int counter; 
+    printf("Odd numbers between 1 to 6\n");  
+    counter = 1;
+    while (counter <= 6) {  
+        printf("%d ", counter);
+        counter = counter + 2;  
+    }  
+       return 0;  
+} 
+ 
