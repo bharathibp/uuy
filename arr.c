a12c0c4787525e77ec5a5922fd817938dc2bb538
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-02-05 15:38:31 +0530

    arr.c

---
+++ arr.c
@@ -0,0 +1,40 @@
+int main()
+{
+    int A[] = {1, 4, 45, 6, 10, -8};
+    int n = 16;
+    int arr_size = 6;
+    
+    if( hasArrayTwoCandidates(A, arr_size, n))
+        printf("Array has two elements with sum 16");
+    else
+        printf("Array doesn't have two elements with sum 16 ");
+ 
+    getchar();
+    return 0;
+}
+void exchange(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a   = *b;
+    *b   = temp;
+}
+ 
+int partition(int A[], int si, int ei)
+{
+    int x = A[ei];
+    int i = (si - 1);
+    int j;
+ 
+    for (j = si; j <= ei - 1; j++)
+    {
+        if(A[j] <= x)
+        {
+            i++;
+            exchange(&A[i], &A[j]);
+        }
+    }
+    exchange (&A[i + 1], &A[ei]);
+    return (i + 1);
+}
+ 
