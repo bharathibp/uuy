21c2f741548b0ee08a1f0e1e0cfc3b8fe6d7feb2
bharathibp <35304908+bharathibp@users.noreply.github.com>
2018-03-07 02:51:50 -0800

    fibona.c

---
+++ fibona.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+	int i,n,a=1,b=0,c=1;
+	printf("ente value");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	printf("%d",c);
+	a=b;
+	b=c;
+	c=a+b;}
+	return 0;
+}
+
+	
